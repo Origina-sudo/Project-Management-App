30b7751764c67f27a30577706166b756b7cb72cf
Origina-sudo <arthurba8585@gmail.com>
2024-07-30 17:13:00 +0000

    Changed button hover from grey to white

--- src/components/ProjectSideBar.jsx
+++ src/components/ProjectSideBar.jsx
@@ -8,7 +8,7 @@ const ProjectSideBar = () => {
             <button className='px-4 py-2 text-ss md:text-base 
             rounded-md bg-stone-700
              text-stone-400 hover:bg-stone-600
-              hover:text-stone-600'>+Add Project</button>
+              hover:text-stone-100'>+Add Project</button>
         </div>
         <ul>
  
