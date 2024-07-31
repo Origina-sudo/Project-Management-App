cc49257b655deff92a215269dacb506e14197742
Origina-sudo <arthurba8585@gmail.com>
2024-07-31 21:10:36 +0000

    Deleted one of the "Add project Button"

--- src/components/ProjectSideBar.jsx
+++ src/components/ProjectSideBar.jsx
@@ -6,13 +6,10 @@ const ProjectSideBar = () => {
     <aside className='w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl '>
         <h2 className='mb-8 font-bold uppercase md:text-xl text-stone-200'>Your Projects</h2>
         <div>
-            <button className='px-4 py-2 text-ss md:text-base 
-            rounded-md bg-stone-700
-             text-stone-400 hover:bg-stone-600
-              hover:text-stone-100'>+Add Project</button>
+        <Button>+Add Projects</Button>
         </div>
         <ul>
-        <Button>+Add Projects</Button>
+    
         </ul>
 
     </aside> 
