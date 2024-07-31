4cfe4f21b0e77bc45bbb745aeff2c84439d4307e
Origina-sudo <arthurba8585@gmail.com>
2024-07-31 21:08:52 +0000

    Changed Rendered components from "<NewProject> to <NoProjectSelected>

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import ProjectSideBar from "./components/ProjectSideBar"
 import NewProject from "./components/NewProject";
-
+import NoProjectSelected from "./components/NoProjectSelected";
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSideBar/>
-      <NewProject/>
+      <NoProjectSelected/>
     </main> 
   );
 }
