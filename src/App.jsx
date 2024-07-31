49ff21472737ef2373ab8ee69b428d9176409578
Origina-sudo <arthurba8585@gmail.com>
2024-07-31 20:22:05 +0000

    Styling Buttons & Inputs with Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ProjectSideBar from "./components/ProjectSideBar"
+import NewProject from "./components/NewProject";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectSideBar/>
+      <NewProject/>
     </main> 
   );
 }
