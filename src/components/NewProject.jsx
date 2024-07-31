904e0259dfd86df748ad6edd9980e19a50bd7b2d
Origina-sudo <arthurba8585@gmail.com>
2024-07-31 20:27:26 +0000

    Finised styling buttons in NewProject COMPO

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -3,10 +3,11 @@ import Input from './Inputs'
 
 const NewProject = () => {
   return (
-    <div>
-        <menu>
-            <li><button>Cancel</button></li>
-            <li><button>Save</button></li>
+    <div className='w-[35rem] mt-16'>
+        <menu className='flex items-center justify-end gap-4 my-4  '>
+            <li><button className='text-stone-800 hover:text-stone-950'>Cancel</button></li>
+            <li><button className='bg-stone-800 text-stone-50
+             hover:bg-stone-950 px-6 py-2 rounded-md'>Save</button></li>
         </menu> 
         <div>
        <Input label="Title"/>
