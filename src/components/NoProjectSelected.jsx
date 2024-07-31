6758cf77f5a209a166c9cecbf1deaf622f78d18c
Origina-sudo <arthurba8585@gmail.com>
2024-07-31 20:44:24 +0000

    Finised creating NPS skelleton & markup

---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NoProjectSelected = () => {
+  return (
+    <div>
+        <img src="" alt="" />
+        <h2>No Project Selected</h2>
+        <p>Select a project or get started with a new One</p>
+        <p>
+            <button>Create New Project</button>
+        </p>
+    </div>
+  )
+}
+
+export default NoProjectSelected
