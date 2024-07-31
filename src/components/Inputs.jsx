088bdf1ebff4d25ea592b76c5a7e1ff91d4ebf9e
Origina-sudo <arthurba8585@gmail.com>
2024-07-31 20:32:13 +0000

    Finished styling Input COMPO

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Input = ({textarea,label,...props}) => {
     return (
-      <p>
-     <label >{label}</label>
-     {textarea ? <textarea {...props}/> : <input {...props} />}
+      <p className='flex flex-col gap-1 my-4'>
+     <label className='text-sm font-bold uppercase text-stone-500' >{label}</label>
+     {textarea ? <textarea className='w-full p-1 border-b-2 
+     rounded-sm border-stone-300 bg-stone-200 text-stone-600
+      focus:outline-none
+       focus:border-stone-600' {...props}/> : <input {...props} />}
       </p>
     )
   }
