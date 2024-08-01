11e7cca32d18c176390d8ec6f2e6653ccbb92b5e
Origina-sudo <arthurba8585@gmail.com>
2024-08-01 11:28:45 +0000

    Creating Dynamic Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import ProjectSideBar from "./components/ProjectSideBar"
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
+
 function App() {
+const [projectstate, setProjectstate] = useState({
+  selectedProjectId: undefined,
+  projects: []
+})
+
+function handleStartAddProject(){
+      setProjectstate(prevSate => {
+       return{
+        selectedProjectId: null,
+        ...prevSate,
+        selectedProjectId : null,
+       }
+      });
+}
+
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSideBar/>
