98aaed19ef5a93e7f5ac790666d731ca3ee58eb5
Origina-sudo <arthurba8585@gmail.com>
2024-08-01 12:26:01 +0000

    Will add validation Later

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
+import { forwardRef } from 'react';
 
 
-
-const Input = ({textarea,label,...props}) => {
+const Input = forwardRef(({textarea,label,...props}, ref) => {
   const classes = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
     return (
       <p className='flex flex-col gap-1 my-4'>
      <label className='text-sm font-bold uppercase text-stone-500' >{label}</label>
-     {textarea ? ( <textarea className={classes} {...props}/> 
+     {textarea ? ( <textarea  ref={ref}className={classes} {...props}/> 
       ) :(
-     <input className={classes} {...props} />
+     <input ref={ref} className={classes} {...props} />
     )}
       </p>
     )
-  }
+  })
    
-  export default Input
+  export default Input;
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -5,20 +5,34 @@ import { useRef } from 'react'
 const NewProject = () => {
 
 const title=useRef();
-const Description=useRef();
-const title=useRef();
+const description=useRef();
+const dueDate=useRef();
+
+function handleSave(event) {
+      const enteredTitle = title.current.value
+      const enteredDescription = description.current.value
+       const enteredDueDate = dueDate.current.value
+
+       //Validation.....
+
+
+       
+}
 
   return (
     <div className='w-[35rem] mt-16'>
         <menu className='flex items-center justify-end gap-4 my-4  '>
             <li><button className='text-stone-800 hover:text-stone-950'>Cancel</button></li>
-            <li><button className='bg-stone-800 text-stone-50
-             hover:bg-stone-950 px-6 py-2 rounded-md'>Save</button></li>
+            <li><button
+            onClick={handleSave} 
+            className='bg-stone-800 text-stone-50
+             hover:bg-stone-950 px-6 
+             py-2 rounded-md'>Save</button></li>
         </menu> 
         <div>
-       <Input label="Title" />
-       <Input label="Description" textarea/>
-       <Input label="Due Date"/>
+       <Input ref={title} label="Title" />
+       <Input ref={description} label="Description" textarea/>
+       <Input  ref={dueDate}label="Due Date"/>
         </div>
         
     </div>
