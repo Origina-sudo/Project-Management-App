8fc16583cc5028fac8b716bc889823948a1f44c3
Origina-sudo <arthurba8585@gmail.com>
2024-08-20 21:37:08 +0000

    Updates

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -33,9 +33,9 @@ onAdd({
   return (
     <>
       <Modal ref={modal} buttonCaption='Okay'> 
-        <h2 className='text-xl font-bold text-stone-500 my-4'>Invalid Input</h2>
-        <p className='text-stone-400 mb-4'>Ooops.. looks like you forgot to enter a value.</p>
-        <p className='text-stone-400 mb-4 '>Please make sure you provide a valid value for every input field.</p>
+        <h2 className='text-xl font-bold text-stone-800 my-4'>Invalid Input</h2>
+        <p className='text-stone-700 mb-4'>Ooops.. looks like you forgot to enter a value.</p>
+        <p className='text-stone-700 mb-4 '>Please make sure you provide a valid value for every input field.</p>
       </Modal>
       <div className='w-[35rem] mt-16'>
         <menu className='flex items-center justify-end gap-4 my-4  '>
