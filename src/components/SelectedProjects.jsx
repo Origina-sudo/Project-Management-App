6880c5b185de6cfa988f294e3792bd5c17b756d4
Origina-sudo <arthurba8585@gmail.com>
2024-08-20 21:56:05 +0000

    Styled Selectted Project

--- src/components/SelectedProjects.jsx
+++ src/components/SelectedProjects.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 
-const SelectedProjects = () => {
+const SelectedProjects = ({ project }) => {
+    
+    const formattedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
+        year: "numeric",
+        month :"short",
+        day: "numeric"
+    })
   return (
-    <div>
-      <header>
-        <div>
-          <h1>TITLE</h1>
-          <button>Delete</button>
+    <div className="w-[35rem] mt-16">
+      <header className="pd-4 md-4 border-b-2 border-stone-300">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold text-stone-600 mb-2">{project.title}</h1>
+          <button className="text-stone-600 hover:text-stone-950">Delete</button>
               </div>
-              <p>DATE</p>
-              <p>DESCRIPTION</p>
+              <p className="mb-4 text-stone-400">{ formattedDate}</p>
+              <p className="text-stone-600 whitespace-pre-wrap">
+                  {project.description}</p>
       </header>
       Task
     </div>
