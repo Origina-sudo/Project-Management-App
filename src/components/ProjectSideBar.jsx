0454414c883e84fcd1415a8b62a0247bb9711620
Origina-sudo <arthurba8585@gmail.com>
2024-08-20 20:35:33 +0000

    updates

--- src/components/ProjectSideBar.jsx
+++ src/components/ProjectSideBar.jsx
@@ -10,9 +10,11 @@ const ProjectSideBar = ({onStartAddProject,projects}) => {
         <Button onClick={onStartAddProject}>+Add Projects</Button>
         </div>
         <ul className='mt-8'>
-        {projects.map(project =><li 
+        {projects.map(project => <li 
         key={project.id}>
-          <button className='w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800'>{project.title}</button>
+          <button className='w-full text-left px-2 py-1 rounded-sm
+           my-1 text-stone-400 hover:text-stone-200
+            hover:bg-stone-800'>{project.title}</button>
         </li>)}
         </ul>
 
