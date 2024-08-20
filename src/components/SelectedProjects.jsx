2d7e3814280be15bd2b134a4855b30a68efc5f0b
Origina-sudo <arthurba8585@gmail.com>
2024-08-20 21:46:18 +0000

    Making projects selecteable

---
+++ src/components/SelectedProjects.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SelectedProjects = () => {
+  return (
+      <div>
+          <header>
+              <h1>TITLE</h1>
+              <button>Delete</button>
+          </header>
+            Task
+
+    </div>
+  )
+}
+
+export default SelectedProjects
