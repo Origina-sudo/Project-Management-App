a7d2a340c0716a100d4e590be91bdcb3ac1e2d42
Origina-sudo <arthurba8585@gmail.com>
2024-08-21 21:57:17 +0000

    Adding functionality to being able to select and highlight selected elements

--- src/components/ProjectSideBar.jsx
+++ src/components/ProjectSideBar.jsx
@@ -17,18 +17,24 @@ const ProjectSideBar = ({
         <Button onClick={onStartAddProject}>+Add Projects</Button>
       </div>
       <ul className="mt-8">
-        {projects.map((project) => (
-          <li key={project.id}>
+        {projects.map((project) => {
+
+          let cssClasses = "w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800"
+          if (project.id === selectedProjectId) {
+            cssClasses += "bg-stone-800 text-stone-200"
+          } else {
+            cssClasses += "text-stone-400"
+          }
+          return (<li key={project.id}>
             <button
+              className={cssClasses}
               onClick={onSelectProject}
-              className="w-full text-left px-2 py-1 rounded-sm
-           my-1 text-stone-400 hover:text-stone-200
-            hover:bg-stone-800"
             >
               {project.title}
             </button>
           </li>
-        ))}
+          );
+        })}
       </ul>
     </aside>
   );
