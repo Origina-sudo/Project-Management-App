f44354f75dc5ad111840f63008c0f8d274ffb719
Origina-sudo <arthurba8585@gmail.com>
2024-08-28 13:16:54 +0000

    Updates

--- src/components/SelectedProjects.jsx
+++ src/components/SelectedProjects.jsx
@@ -14,7 +14,7 @@ const SelectedProjects = ({ project }) => {
           <h1 className="text-3xl font-bold text-stone-600 mb-2">{project.title}</h1>
           <button className="text-stone-600 hover:text-stone-950">Delete</button>
               </div>
-              <p className="mb-4 text-stone-400">{ formattedDate}</p>
+              <p className="mb-4 text-stone-400">{formattedDate}</p>
               <p className="text-stone-600 whitespace-pre-wrap">
                   {project.description}</p>
       </header>
