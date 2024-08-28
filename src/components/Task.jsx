d0354a6d75128ebaee5645b18449f6987a70828b
Origina-sudo <arthurba8585@gmail.com>
2024-08-28 14:51:19 +0000

    Peop Drilling

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Newtask from './Newtask'
-const Task = ({onAdd, onDelete}) => {
+const Task = ({ tasks ,onAdd, onDelete}) => {
   return (
       <section>
           <h2 className='text-2xl font-bold text-stone-700 mb-4'>Tasks</h2>
           <Newtask onAdd={onAdd} />
           {/* Will render only conditionally */}
-          <p className='text-stone-800 my-4'>This Project does not have any tasks yet</p> 
-          <ul></ul>
+          {tasks.lenght === 0 &&
+              (<p className='text-stone-800 my-4'>This Project does not have any tasks yet.
+              </p>)} 
+          {tasks.leght > 0 && <ul>
+              {tasks.map((task) => <li key={task.id}>{task.text}</li>)}
+          </ul>}
+         
      </section>
       
   )
