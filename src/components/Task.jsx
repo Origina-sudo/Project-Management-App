9eea49cfc2088415d8f6cfab71fbfaba51dc4746
Origina-sudo <arthurba8585@gmail.com>
2024-08-28 13:54:32 +0000

    Finished creating Task Schema

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Task = () => {
   return (
       <section>
-          
+          <h2>Task</h2>
+          NEW TASK
+          {/* Will render only conditionally */}
+          <p>This Project does not have any tasks yet</p> 
+          <ul></ul>
      </section>
       
   )
