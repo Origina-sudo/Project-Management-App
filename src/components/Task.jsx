ba1c340e538558fa193ccb7e7075103dfec83f81
Origina-sudo <arthurba8585@gmail.com>
2024-08-28 14:07:01 +0000

    Dynamic styling

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -6,7 +6,7 @@ const Task = () => {
           <h2 className='text-2xl font-bold text-stone-700 mb-4'>Tasks</h2>
           <Newtask/>
           {/* Will render only conditionally */}
-          <p className='text-stone-800 mt-4'>This Project does not have any tasks yet</p> 
+          <p className='text-stone-800 my-4'>This Project does not have any tasks yet</p> 
           <ul></ul>
      </section>
       
