cbc1a09ac2cc50aadafcd7055605fa71d6159e83
Origina-sudo <arthurba8585@gmail.com>
2024-08-28 13:35:18 +0000

    Connecting onClick to onDelete

--- src/components/SelectedProjects.jsx
+++ src/components/SelectedProjects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const SelectedProjects = ({ project }) => {
+const SelectedProjects = ({ project, onDelete }) => {
     
   const formattedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
     year: "numeric",
@@ -12,7 +12,7 @@ const SelectedProjects = ({ project }) => {
       <header className="pd-4 md-4 border-b-2 border-stone-300">
         <div className="flex items-center justify-between">
           <h1 className="text-3xl font-bold text-stone-600 mb-2">{project.title}</h1>
-          <button className="text-stone-600 hover:text-stone-950">Delete</button>
+          <button className="text-stone-600 hover:text-stone-950" onClick={onDelete}>Delete</button>
               </div>
               <p className="mb-4 text-stone-400">{formattedDate}</p>
               <p className="text-stone-600 whitespace-pre-wrap">
