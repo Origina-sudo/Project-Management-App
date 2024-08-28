c16255cf84395e78600918ebb5485b16f29c4d99
Origina-sudo <arthurba8585@gmail.com>
2024-08-28 14:11:29 +0000

    Adding Logice to Newtask Components making the input functionably active

--- src/components/Newtask.jsx
+++ src/components/Newtask.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useState } from 'react'
 
 const Newtask = () => {
+    const [enteredTask, setEnteredTask] = useState()
+    
+    function handleChange(event) {
+        setEnteredTask(event.target.value);
+    }
+
+
   return (
       <div className='flex items-center gap-4'>
-          <input type="text"  className='w-64 px-2 py-1 rounded-sm bg-stone-200'/>
+          <input type="text" className='w-64 px-2 py-1 rounded-sm bg-stone-200'
+              onChange={handleChange}
+          value={enteredTask}/>
           <button className='text-stone-700  hover:text-stone-950'>Add Task</button>
     </div>
   )
