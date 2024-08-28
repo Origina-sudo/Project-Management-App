12632f79265501259517ab21a705f11752ed9fe6
Origina-sudo <arthurba8585@gmail.com>
2024-08-28 14:05:35 +0000

    Importing Newtask to Task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import Newtask from './Newtask'
 const Task = () => {
   return (
       <section>
           <h2 className='text-2xl font-bold text-stone-700 mb-4'>Tasks</h2>
-          NEW TASK
+          <Newtask/>
           {/* Will render only conditionally */}
           <p className='text-stone-800 mt-4'>This Project does not have any tasks yet</p> 
           <ul></ul>
