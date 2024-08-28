46dc480c9cb090f53978482c9510cb4b612f8eac
Origina-sudo <arthurba8585@gmail.com>
2024-08-28 14:04:45 +0000

    Styling

--- src/components/Newtask.jsx
+++ src/components/Newtask.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Newtask = () => {
   return (
       <div className='flex items-center gap-4'>
-          <input type="text"  className='w-64 px-2 py-1 rounded-sm'/>
-          <button>Add Task</button>
+          <input type="text"  className='w-64 px-2 py-1 rounded-sm bg-stone-200'/>
+          <button className='text-stone-700  hover:text-stone-950'>Add Task</button>
     </div>
   )
 }
