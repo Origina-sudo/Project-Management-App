0574c8b2d7b5882d0b646c40742fc431e8e136b5
Origina-sudo <arthurba8585@gmail.com>
2024-08-28 14:33:46 +0000

    PropDrilling

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Newtask from './Newtask'
-const Task = () => {
+const Task = ({onAdd, onDelete}) => {
   return (
       <section>
           <h2 className='text-2xl font-bold text-stone-700 mb-4'>Tasks</h2>
-          <Newtask/>
+          <Newtask onAdd={onAdd} />
           {/* Will render only conditionally */}
           <p className='text-stone-800 my-4'>This Project does not have any tasks yet</p> 
           <ul></ul>
