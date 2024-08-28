3697e87d7c56dcc85d7e29dcd9db28010d2a2299
Origina-sudo <arthurba8585@gmail.com>
2024-08-28 13:55:24 +0000

    Styling component Schema

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Task = () => {
   return (
       <section>
-          <h2>Task</h2>
+          <h2 className='text-2xl font-bold text-stone-700 mb-4'>Task</h2>
           NEW TASK
           {/* Will render only conditionally */}
           <p>This Project does not have any tasks yet</p> 
