e0c35fdaa044038f5ac889c50b07f68434dcef16
Origina-sudo <arthurba8585@gmail.com>
2024-08-28 14:52:53 +0000

    Prop Drilling

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import Newtask from './Newtask'
-const Task = ({ tasks ,onAdd, onDelete}) => {
+import React from "react";
+import Newtask from "./Newtask";
+const Task = ({ tasks, onAdd, onDelete }) => {
   return (
-      <section>
-          <h2 className='text-2xl font-bold text-stone-700 mb-4'>Tasks</h2>
-          <Newtask onAdd={onAdd} />
-          {/* Will render only conditionally */}
-          {tasks.lenght === 0 &&
-              (<p className='text-stone-800 my-4'>This Project does not have any tasks yet.
-              </p>)} 
-          {tasks.leght > 0 && <ul>
-              {tasks.map((task) => <li key={task.id}>{task.text}</li>)}
-          </ul>}
-         
-     </section>
-      
-  )
-}
+    <section>
+      <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
+      <Newtask onAdd={onAdd} />
+      {/* Will render only conditionally */}
+      {tasks.lenght === 0 && (
+        <p className="text-stone-800 my-4">
+          This Project does not have any tasks yet.
+        </p>
+      )}
+      {tasks.leght > 0 && (
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>
+              <span>{task.text}</span>
+              <button>Clear</button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
+};
 
-export default Task
+export default Task;
